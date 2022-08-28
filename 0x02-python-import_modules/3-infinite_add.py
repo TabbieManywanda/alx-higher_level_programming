74a6541027ed994b010c7dd22bb9ccea4dc68ead
TabbieManywanda <achiengtabitha@gmail.com>
2022-08-28 11:09:38 -0700

    Prints result of the addition of all the arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    nargs = len(sys.argv)
+
+    total = 0
+    for i in range(1, nargs):
+        total += int(sys.argv[i])
+    print(total)
