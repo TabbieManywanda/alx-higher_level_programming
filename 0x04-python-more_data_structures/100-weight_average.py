26a4ad11f9c78b7c338bc7c403077622f309fea9
TabbieManywanda <achiengtabitha@gmail.com>
2022-08-31 12:44:31 -0700

    Returns weighted average of all integers tuple - Rectification

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -6,7 +6,7 @@ def weight_average(my_list=[]):
     result1 = 0
     result2 = 0
     for i, z in my_list:
-        result1 = i * z
+        result1 += i * z
         result2 += z
 
     return result1 / result2
