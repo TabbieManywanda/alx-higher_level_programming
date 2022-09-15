6e2172c6cc9476611e3044d8bc33c82a1cd4f1df
TabbieManywanda <achiengtabitha@gmail.com>
2022-09-15 13:01:58 -0700

    Still defining a square with more attributes though

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,12 +8,7 @@ class Square:
     def __init__(self, __size=0):
         self.__size = __size
 
-        try:
-            if type(__size) is not int:
-                print("size must be an integer")
-            if __size < 0:
-                print("size must be >= 0")
-        except TypeError:
-            print()
-        except ValueError:
-            print()
+        if type(__size) is not int:
+            raise TypeError("size must be an integer")
+        if __size < 0:
+            raise ValueError("size must be >= 0")
