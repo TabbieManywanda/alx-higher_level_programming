630ad6ca3379e5811ad05a569727eb111de498cf
TabbieManywanda <achiengtabitha@gmail.com>
2022-09-16 08:27:21 -0700

    Printing square using '#' character

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Defining a class Square; printing out a square now added"""
+
+
+class Square:
+    """class Square"""
+    def __init__(self, __size=0):
+        self.__size = __size
+        if __size < 0:
+            raise ValueError("size must be >= 0")
+        if type(__size) is not int:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        return self__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+
+    def my_print(self):
+        for i in range(self.__size):
+            for j in range(self.__size):
+                if self.__size == 0:
+                    print()
+                else:
+                    print("#", end="")
+            print()
