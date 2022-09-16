6b2cf1d7b65b57068462c95494604e15bcb5653a
TabbieManywanda <achiengtabitha@gmail.com>
2022-09-16 12:34:19 -0700

    Square position

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Adding position attribute to the class"""
+
+
+class Square:
+    """class Square"""
+    def __init__(self, __size=0, __position=(0, 0)):
+        self.__size = __size
+        self.__position = __position
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        self.__position = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for y in range(self.__position[1]):
+            print()
+        for x in range(self.__size):
+            print(" " * self.__position, end="")
+            for f in range(self.__size):
+                print("#", end="")
+            print()
