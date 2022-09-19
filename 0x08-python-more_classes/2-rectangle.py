b35004db41f937296926f89940f6573c0f1c01c9
TabbieManywanda <achiengtabitha@gmail.com>
2022-09-19 13:06:28 -0700

    Area and perimeter methods added to class Rectangle - Edit

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -6,20 +6,11 @@ class Rectangle:
     """Class Rectangle"""
     def __init__(self, width=0, height=0):
         """Initialising class Rectangle"""
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
         self.__width = width
-
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
         self.__height = height
 
     def area(self):
-        return self.__width * self.__height
+        return (self.__width * self.__height)
 
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
