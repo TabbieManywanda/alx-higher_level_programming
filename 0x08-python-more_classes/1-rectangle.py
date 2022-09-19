c1d1106785096744eff188bbb51673cb8f12b5ec
TabbieManywanda <achiengtabitha@gmail.com>
2022-09-19 11:25:44 -0700

    Adding attributes to the class Rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Python class Rectangle having width and height 
+attributes added and privatised.
+The arguments for both methods have to be positive 
+integers."""
+
+
+class Rectangle:
+    """Width and height have to be positive integers"""
+    def __init__(self, width=0, height=0):
+        """Initialising the class"""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        self.__width = value
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        self.__height = value
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
