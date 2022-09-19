90a8b716e8fb190b29067cac55d63d22d4631dbb
TabbieManywanda <achiengtabitha@gmail.com>
2022-09-19 11:38:58 -0700

    Adding attributes to the class Rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -21,7 +21,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
-        else:    
+        else:
             self.__width = value
 
     @property
@@ -36,5 +36,5 @@ class Rectangle:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
-        else:    
+        else:
             self.__height = value
