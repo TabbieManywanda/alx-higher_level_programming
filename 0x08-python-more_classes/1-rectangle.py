ce04d4a8c1e4feee2fd15ebf2d6494f095d44456
TabbieManywanda <achiengtabitha@gmail.com>
2022-09-22 05:43:13 -0700

    Edit

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,16 +5,16 @@
 class Rectangle:
     """Width and height have to be positive integers"""
     def __init__(self, width=0, height=0):
-        if height < 0:
-            raise ValueError("height must be >= 0")
         if type(height) is not int:
             raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self.__height = height
 
-        if width < 0:
-            raise ValueError("width must be >= 0")
         if type(width) is not int:
             raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
         self.__width = width
 
     @property
