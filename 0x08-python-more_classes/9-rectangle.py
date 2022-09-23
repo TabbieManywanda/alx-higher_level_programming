31a8e7e1f8ad10b8ee48e5532dddbea1b2b9a291
TabbieManywanda <achiengtabitha@gmail.com>
2022-09-23 08:22:16 -0700

    Returns a square Rectangle instance - Adding exceptions

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -89,4 +89,8 @@ class Rectangle:
     def square(cls, size=0):
         """Returns a new Rectangle instance with
         width == height == size"""
+        if type(size) is not int:
+            raise TypeError("width must be an integer")
+        if size < 0:
+            raise ValueError("width must be >= 0")
         return cls(size, size)
