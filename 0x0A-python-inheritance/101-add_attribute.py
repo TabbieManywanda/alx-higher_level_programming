294888da8f50b55f416cf1dac5d8c9afc75aa632
TabbieManywanda <achiengtabitha@gmail.com>
2022-09-27 13:43:02 -0700

    Adding documentation

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,6 +4,8 @@ to object if possible"""
 
 
 def add_attribute(*args):
+    """Adds new attribute to object if
+    possible"""
     if "main" in str(type(args[0])):
         setattr(args[0], args[1], args[2])
     else:
