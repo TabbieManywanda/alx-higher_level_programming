d3d3b0a3a70f1b53bc36d6c078441ec3074bab5a
TabbieManywanda <achiengtabitha@gmail.com>
2022-09-27 12:40:05 -0700

    Adding a sub class Square inheriting form Rectangle

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Adding a sub class Square that inherits from
+class Rectangle"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Defines a sub class Square
+    inheriting from super class Rectangle"""
+    def __init__(self, size):
+        """Initializing instance, args =
+        size"""
+        BaseGeometry.integer_validator(self, "size", size)
+        self.__size = size
+
+    def __str__(self):
+        """Description of the square"""
+        return ("[Rectangle] {}/{}".format(self.__size, self.__size))
+
+    def area(self):
+        """Returns the area of a square"""
+        return self.__size ** 2
