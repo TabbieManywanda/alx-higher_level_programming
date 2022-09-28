b5b9c78b9a47d14ebb4ea9c1f2eedeee05f4655e
TabbieManywanda <achiengtabitha@gmail.com>
2022-09-28 13:03:27 -0700

    Edit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,6 +2,7 @@
 """Adds all arguments to a Python list and save
 them to a file"""
 
+
 import json
 from sys import argv
 
@@ -10,10 +11,10 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 try:
-    deserial = load_from_json_file(filename)
-except FileNotFoundError:
-    deserial = []
-finally:
-    for x in argv[1:]:
-        deserial.append(str(x))
-    save_to_json_file(deserial, filename)
+    pylist = load_from_json_file(filename)
+except:
+    pylist = []
+
+for x in argv[1:]:
+    pylist.append(x)
+save_to_json_file(pylist, filename)
