e9e397c1e26a56b0df04da8c92eb0d67b345f043
TabbieManywanda <achiengtabitha@gmail.com>
2022-09-28 10:39:36 -0700

    Moduled documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,7 @@ and prints it out to stdout"""
 
 
 def read_file(filename=""):
+    """Reads a text file and prints it
+    to stdout"""
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read())
