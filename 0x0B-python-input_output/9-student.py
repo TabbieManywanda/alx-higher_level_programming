2a449c4310a66db2001359d21c770d1e29e9031a
TabbieManywanda <achiengtabitha@gmail.com>
2022-09-28 12:21:06 -0700

    Edit

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -9,8 +9,8 @@ class Student:
     Student instance"""
     def __init__(self, first_name, last_name, age):
         """Initializing Student instance"""
-        self.firstname = first_name
-        self.lastname = last_name
+        self.first_name = first_name
+        self.last_name = last_name
         self.age = age
 
     def to_json(self):
