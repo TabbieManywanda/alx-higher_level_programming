d670da9222c770df233ea1b6c2b78de77c2b83ce
TabbieManywanda <achiengtabitha@gmail.com>
2022-09-29 06:40:22 -0700

    Class Student

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""class Student defined"""
+
+
+class Student:
+    """class with public instance attributes:
+    firstname, lastname and age and a public method
+    that retrieves a dictionary representation of a
+    Student instance"""
+    def __init__(self, first_name, last_name, age):
+        """Initializing Student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation
+        of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+        d = {}
+        for x in attrs:
+            try:
+                d[x] = self.__dict__[x]
+            except KeyError:
+                pass
+        return d
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the
+        Student instance"""
+        for key in json:
+            try:
+                setattr(self, key, json[key])
+            except KeyError:
+                pass
