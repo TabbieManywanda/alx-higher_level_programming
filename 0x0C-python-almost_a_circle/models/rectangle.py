10dc5ff786fb4a00e2bdf003577d9248abc46958
TabbieManywanda <achiengtabitha@gmail.com>
2022-10-04 06:09:42 -0700

    Method documentations added

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -21,32 +21,40 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """getter for width"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """width setter"""
         self.__width = value
 
     @property
     def height(self):
+        """height getter"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """height setter"""
         self.__height = value
 
     @property
     def x_value(self):
+        """x getter"""
         return self.__x
 
     @x_value.setter
     def x_value(self, value):
+        """x setter"""
         self.__x = value
 
     @property
     def y_value(self):
+        """y getter"""
         return self.__y
 
     @y_value.setter
     def y_value(self, value):
+        """y setter"""
         self.__y = value
