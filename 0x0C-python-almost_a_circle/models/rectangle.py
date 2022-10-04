4bd35df630becfcfa71ef501b60ea5c6ae6f369d
TabbieManywanda <achiengtabitha@gmail.com>
2022-10-04 07:14:12 -0700

    Error rectification

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -78,7 +78,7 @@ class Rectangle(Base):
         if type(value) is not int:
             raise TypeError("x must be an integer")
         if value < 0:
-            raise ValueError("x must be > 0")
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -90,9 +90,9 @@ class Rectangle(Base):
     def y(self, value):
         """y setter"""
         if type(value) is not int:
-            raise TypeError("x must be an integer")
+            raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("x must be > 0")
+            raise ValueError("y must be >= 0")
         self.__y = value
 
     def area(self):
