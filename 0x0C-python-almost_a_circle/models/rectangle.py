5f273890686ddaeebad8ac61588d69e8e0064f0b
TabbieManywanda <achiengtabitha@gmail.com>
2022-10-04 08:46:38 -0700

    Adding __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -109,4 +109,6 @@ class Rectangle(Base):
     def __str__(self):
         """Returns string in format of
         [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(id, x, y, width, height)
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,
+                                                       self.__y, self.__width,
+                                                       self.__height)
