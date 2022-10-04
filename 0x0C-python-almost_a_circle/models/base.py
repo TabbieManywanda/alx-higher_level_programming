67858c59ed2d3c621dd9b61201abebe23cb850ab
TabbieManywanda <achiengtabitha@gmail.com>
2022-10-04 05:41:45 -0700

    Edit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -10,7 +10,7 @@ class Base:
     def __init__(self, id=None):
         """Initializing instance"""
         if id is None:
-            Base.__nb_objects =+ 1
+            Base.__nb_objects += 1
             self.id = self.__nb_objects
         else:
             self.id = id
