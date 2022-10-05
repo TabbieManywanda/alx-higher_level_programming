7814cc1fbb0ccf09a9bbf14f54465ca126a0884a
TabbieManywanda <achiengtabitha@gmail.com>
2022-10-05 10:42:14 -0700

    Edit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,7 +22,7 @@ class Base:
     def to_json_string(list_dictionaries):
         """Returns the JSON string representation
         of list_dictionaries"""
-        if len(list_dictionaries) == 0 or (list_dictionaries is None):
-            list_dictionaries = "[]"
+        if list_dictionaries is None:
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
