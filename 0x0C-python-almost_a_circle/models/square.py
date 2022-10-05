5e1af4c67c2b1aa4b2451004c8369f7a73ac8ef0
TabbieManywanda <achiengtabitha@gmail.com>
2022-10-05 08:15:34 -0700

    update() method added to class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -29,3 +29,25 @@ class Square(Rectangle):
         """Returns string defining Square properties"""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x,
                                                  self.y, self.width)
+
+    def update(self, *args, **kwargs):
+        """Assigns attributes"""
+        if len(args) != 0:
+            for count in range(len(args)):
+                if count == 0 :
+                    self.id = args[count]
+                elif count == 1:
+                    self.size = args[count]
+                elif count == 2:
+                    self.x = args[count]
+                elif count == 3:
+                    self.y = args[count]
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.size = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
