bb9587105fbdf80e06fcc8c991ab0ebaec930928
TabbieManywanda <achiengtabitha@gmail.com>
2022-10-06 10:18:04 -0700

    Rectangle unittest

---
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Unittest for class Rectangle"""
+
+
+import unittest
+
+
+class TestRectangle(unittest.TestCase):
+    """unittest for Rectangle"""
+    def test_two_parameters(self):
+
