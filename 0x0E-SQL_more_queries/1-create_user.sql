fbac9d3fcaf78b0f1f00c58854c5894c82167eb8
TabbieManywanda <achiengtabitha@gmail.com>
2022-11-07 03:41:40 -0800

    Creating user with all grants

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,5 +2,5 @@
 -- user should have all privileges
 -- user password set
 -- if user exists, script should not fail
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
