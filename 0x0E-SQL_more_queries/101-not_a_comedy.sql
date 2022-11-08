3ce1bee2514ed5480787ce914862d8ba58ff0892
TabbieManywanda <achiengtabitha@gmail.com>
2022-11-08 03:36:17 -0800

    lists all shows without the genre Comedy

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,13 @@
+-- lists all shows without the genre Comedy
+-- display: tv_shows.title
+-- results sorted in ascending order by the show title
+SELECT tv_shows.title
+FROM tv_shows
+WHERE tv_shows.title NOT IN(
+	SELECT tv_shows.title
+	FROM tv_shows INNER JOIN tv_show_genres
+	ON tv_shows.id = tv_show_genres.show_id
+	INNER JOIN tv_genres
+	ON tv_genres.id = tv_show_genres.genre_id
+	WHERE tv_genres.name = 'Comedy')
+ORDER BY tv_shows.title;
