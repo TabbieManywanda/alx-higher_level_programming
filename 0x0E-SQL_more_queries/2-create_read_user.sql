f978a8f09560fbb46b97f4d2054f4e9a391cab4f
TabbieManywanda <achiengtabitha@gmail.com>
2022-11-07 23:24:53 -0800

    Creating database and user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- creating database and user
+-- user should have only select privilege in database created
+-- script should not fail if database and user already exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
