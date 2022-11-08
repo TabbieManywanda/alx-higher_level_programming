c803413467eb571d54ccf2ad41f46186ab609539
TabbieManywanda <achiengtabitha@gmail.com>
2022-11-08 00:13:12 -0800

    Listing all cities of California in the database sorted in ascending order by id of cities table

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- lists all cities of California found in the hbtn_0d_usa database
+-- results sorted in ascending order by cities.id
+SELECT id, name
+FROM cities
+WHERE state_id = (
+	SELECT id
+	FROM states
+	WHERE name = 'California')
+ORDER BY id ASC;
