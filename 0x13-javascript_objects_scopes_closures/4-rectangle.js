56571907d6fabdc0fdaa881112e301964873584d
TabbieManywanda <achiengtabitha@gmail.com>
2022-12-01 08:08:16 -0800

    Defining a rectangle

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+class Rectangle {
+  console (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const exes = 'X'.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(exes);
+    }
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+}
+
+module.exports = Rectangle;
