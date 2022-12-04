caf73b05c70870caa091d5450a3f85e271dbbb33
TabbieManywanda <achiengtabitha@gmail.com>
2022-12-04 07:33:30 -0800

    Edit

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
 const list1 = require('./100-data');
-const newList = list1.list.map((element, index) => element * index);
+const newList = list1.map((element, index) => element * index);
 console.log(list1);
 console.log(newList);
