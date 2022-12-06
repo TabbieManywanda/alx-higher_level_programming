95790befb75f034783a06e541873d8743ef8200c
TabbieManywanda <achiengtabitha@gmail.com>
2022-12-06 04:43:34 -0800

    safe from MySQL injections

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''safe from MySQL injections'''
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute('''SELECT * FROM states WHERE name = %s ORDER BY id ASC''',
+                (sys.argv[4],))
+    fetchdata = cur.fetchall()
+    for x in fetchdata:
+        print(x)
+    cur.close()
+    db.close()
