785b35978d65a03e09a6034c0b44b035a50b3081
TabbieManywanda <achiengtabitha@gmail.com>
2022-12-16 07:39:05 -0800

    fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''fetches `https://alx-intranet.hbtn.io/status`'''
+
+import urllib.request
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(
+            'https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
